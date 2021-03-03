c8fbed13ba1b2ebe65ee4d8a3ebe05f2cfbae1e9
Marko1212 <shihonage7@gmail.com>
2021-03-03 13:58:58 +0100

    update

---
+++ spring_inicio_jpa_v1/src/test/java/com/formation/spring_inicio_jpa_v1/SpringInicioJpaV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formation.spring_inicio_jpa_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInicioJpaV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
