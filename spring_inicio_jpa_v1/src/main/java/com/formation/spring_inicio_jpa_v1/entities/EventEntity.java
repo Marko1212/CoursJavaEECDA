06c3cbc7971af64f77f7f2d6ea113687ff1a83fa
Marko1212 <shihonage7@gmail.com>
2021-03-03 14:22:55 +0100

    update

---
+++ spring_inicio_jpa_v1/src/main/java/com/formation/spring_inicio_jpa_v1/entities/EventEntity.java
@@ -0,0 +1,27 @@
+package com.formation.spring_inicio_jpa_v1.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Calendar;
+
+@Data
+@NoArgsConstructor
+@Entity
+
+public class EventEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(nullable = false)
+    private String title;
+    private String description;
+    @Temporal(TemporalType.DATE)
+    @ToString.Exclude
+    private Calendar beginDate;
+    private boolean allDay;
+
+
+}
