4c9139a7f2445ef731a7fd4775ed26b8f7b346d4
Marko1212 <shihonage7@gmail.com>
2021-03-03 14:47:48 +0100

    update

--- spring_inicio_jpa_v1/src/main/java/com/formation/spring_inicio_jpa_v1/services/EventService.java
+++ spring_inicio_jpa_v1/src/main/java/com/formation/spring_inicio_jpa_v1/services/EventService.java
@@ -1,8 +1,12 @@
 package com.formation.spring_inicio_jpa_v1.services;
 
+import com.formation.spring_inicio_jpa_v1.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EventService {
     
+
+
 }
