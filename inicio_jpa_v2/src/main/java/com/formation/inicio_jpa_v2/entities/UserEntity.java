0581c02517637a7bf8e51142c0ce40c77f0dc9ac
Marko1212 <shihonage7@gmail.com>
2021-03-05 09:31:49 +0100

    update

--- inicio_jpa_v2/src/main/java/com/formation/inicio_jpa_v2/entities/UserEntity.java
+++ inicio_jpa_v2/src/main/java/com/formation/inicio_jpa_v2/entities/UserEntity.java
@@ -1,5 +1,9 @@
 package com.formation.inicio_jpa_v2.entities;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,14 +20,21 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Pattern(regexp = "^[a-z0-9À-ÿ]{3,100}$",
+            flags= {Pattern.Flag.CASE_INSENSITIVE},
+            message="Tu ne peux mettre que 3 à 100 lettres / chiffres. Tu n'as pas le droit de mettre des caractères spéciaux.")
     private String login;
+
+    @Size(min = 8, max = 64)
+    @NotNull
     private String pass;
+
+    @Email(message = "Ton email n'est pas valide")
+    @NotNull
     private String email;
 
-    @OneToMany(mappedBy="userEntity", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "userEntity", cascade = CascadeType.ALL)
     private List<EventEntity> eventList;
-
-
 
 
 }
