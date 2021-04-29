371d7671ab734630343c57d03cc312c969c912a1
Marko1212 <shihonage7@gmail.com>
2021-04-29 10:18:02 +0200

    suite

--- SpringWebflix/src/main/java/com/formation/springwebflix/services/FileService.java
+++ SpringWebflix/src/main/java/com/formation/springwebflix/services/FileService.java
@@ -1,11 +1,34 @@
 package com.formation.springwebflix.services;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 public class FileService {
 
 	@Value("${app.upload.dir:${user.home}}")
 	private String uploadDir;
-	
-	
+
+	public String uploadfile(MultipartFile file) {
+		
+		
+		try {
+			Path copyLocation = Paths.get(
+					uploadDir + File.separator + StringUtils.cleanPath(file.getOriginalFilename())
+					);
+			Files.copy(file.getInputStream(), copyLocation, StandardCopyOption.REPLACE_EXISTING);
+			return copyLocation.toString();
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	return null;
+	}
 }
