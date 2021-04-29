4bd7b4b27d97c549ce3bf7197fcd456935f417aa
Marko1212 <shihonage7@gmail.com>
2021-04-29 09:04:13 +0200

    update

--- SpringWebflix/src/main/java/com/formation/springwebflix/controllers/AdminMovieController.java
+++ SpringWebflix/src/main/java/com/formation/springwebflix/controllers/AdminMovieController.java
@@ -1,5 +1,7 @@
 package com.formation.springwebflix.controllers;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.formation.springwebflix.entities.MovieEntity;
 
@@ -18,21 +22,28 @@ public class AdminMovieController {
 	@GetMapping("")
 	public String getAddMovie(Model model) {
 		model.addAttribute("page", "movie/add");
-		model.addAttribute("movie", new MovieEntity());
+		MovieEntity mov = new MovieEntity();
+		mov.setTitle("coucou");
+		mov.setDescription("coucou");
+		mov.setDuration(20);
+		mov.setReleaseDate(new Date());
+		model.addAttribute("movie", mov);
 		return "index";
 	}
 	
 	@PostMapping("")
 	public String postAddMovie(Model model, @Validated @ModelAttribute(name="movie") MovieEntity movie, 
-			BindingResult movieBindingResult) 
+			BindingResult movieBindingResult,
+			@RequestParam(name="file") MultipartFile file)
 	{
-		
-		if (!movieBindingResult.hasErrors()) {
+		System.out.println(file.isEmpty());
+		if (!movieBindingResult.hasErrors() && !file.isEmpty()) {
 			// TODO sauvegarde en BDD
 			System.out.println(movie);
 			return "redirect:/";
 		}
 		model.addAttribute("page", "movie/add");
+		model.addAttribute("fileError", "movie.add.form.file.empty.message");
 		System.out.println(movieBindingResult.getAllErrors());
 		return "index";
 	}
