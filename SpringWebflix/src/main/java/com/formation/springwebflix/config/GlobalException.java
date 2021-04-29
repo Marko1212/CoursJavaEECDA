7413a688bacadf57e13894b9bd6171a5468d3fd0
Marko1212 <shihonage7@gmail.com>
2021-04-29 09:54:06 +0200

    updates

--- SpringWebflix/src/main/java/com/formation/springwebflix/config/GlobalException.java
+++ SpringWebflix/src/main/java/com/formation/springwebflix/config/GlobalException.java
@@ -1,7 +1,13 @@
 package com.formation.springwebflix.config;
 
+import java.util.Locale;
+
 import javax.naming.SizeLimitExceededException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -9,9 +15,20 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @ControllerAdvice
 public class GlobalException {
 	
+	private final MessageSource messageSource;
+	
+	@Autowired
+	public GlobalException(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+	
+	@Value("${spring.servlet.multipart.max-file-size}")
+	private String maxFileSize;	
+
 	@ExceptionHandler(SizeLimitExceededException.class)
 	public String handleMultipartSizeMaxException(RedirectAttributes redirectAttributes) {
-		redirectAttributes.addFlashAttribute("fileError", "movie.add.form.file.too.big.message");
+		String[] args = {maxFileSize};
+		redirectAttributes.addFlashAttribute("fileError",messageSource.getMessage("movie.add.form.file.too.big.message", args, LocaleContextHolder.getLocale()));
 		return "redirect:/admin/movie";
 	}
 
