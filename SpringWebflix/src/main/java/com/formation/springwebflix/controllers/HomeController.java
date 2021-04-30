91d733bfde3e48ab07972e7f3a01ec65ea4c018a
Marko1212 <shihonage7@gmail.com>
2021-04-30 11:37:47 +0200

    movies

--- SpringWebflix/src/main/java/com/formation/springwebflix/controllers/HomeController.java
+++ SpringWebflix/src/main/java/com/formation/springwebflix/controllers/HomeController.java
@@ -1,15 +1,27 @@
 package com.formation.springwebflix.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.formation.springwebflix.services.MovieService;
+
 @Controller
 public class HomeController {
 
+	private final MovieService movieService;
+	
+	@Autowired
+	public HomeController(MovieService movieService) {
+		this.movieService = movieService;
+	}
+	
+	
 	@GetMapping("/")
 	public String home(Model model) {
-		model.addAttribute("title", "<b>Home Page</b>");
+		model.addAttribute("page", "home");
+		model.addAttribute("movies", movieService.findAll());
 		return "index";
 	}
 	
